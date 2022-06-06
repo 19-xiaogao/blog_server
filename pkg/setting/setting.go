c77ef22dada80e608cd7df830d67df8f844b5ae3
xiaolong <longjiuwei999@163.com>
2022-06-06 16:27:20 +0800

    add viper package and  setting global variable

---
+++ pkg/setting/setting.go
@@ -0,0 +1,49 @@
+package setting
+
+import (
+	"github.com/spf13/viper"
+	"time"
+)
+
+type ServerSettingS struct {
+	RunMode      string
+	HttpPort     string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+type DatabaseSettingS struct {
+	DBType       string
+	UserName     string
+	Password     string
+	Host         string
+	DBName       string
+	TablePrefix  string
+	Charset      string
+	ParseTime    bool
+	MaxIdleConns int
+	MaxOpenConns int
+}
+
+type Setting struct {
+	vp *viper.Viper
+}
+
+func NewSetting() (*Setting, error) {
+	vp := viper.New()
+	vp.AddConfigPath("configs/")
+	vp.SetConfigName("config")
+	vp.SetConfigType("yaml")
+	err := vp.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+	return &Setting{vp: vp}, err
+}
+
+func (s *Setting) ReadSection(k string, v interface{}) error {
+	err := s.vp.UnmarshalKey(k, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
