a5cc64fbbe3805562b6f72c00964d43707807001
xiaolong <longjiuwei999@163.com>
2022-06-06 16:28:57 +0800

    write main.go file start server and connecnt mysql database

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"xiaolong_blog/global"
+	"xiaolong_blog/internal/model"
+	"xiaolong_blog/internal/routers"
+	"xiaolong_blog/pkg/setting"
+)
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+
+	err = setupDBEngine()
+	if err != nil {
+		log.Fatalf("init.setupDBEngine err: %v", err)
+	}
+}
+
+func main() {
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":" + global.ServerSetting.HttpPort,
+		Handler:        router,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func setupDBEngine() error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	return nil
+}
