5eb9eb97325a53ae9da97bdcbaf71e1c706959f0
xiaolong <longjiuwei999@163.com>
2022-06-07 22:34:05 +0800

    write article update and delete function

--- internal/dao/article.go
+++ internal/dao/article.go
@@ -2,7 +2,20 @@ package dao
 
 import "xiaolong_blog/internal/model"
 
+// 计算所有文章的总数
 func (d *Dao) CountArticle() (int, error) {
 	article := model.Article{}
 	return article.AllCount(d.engine)
+}
+
+func (d *Dao) UpdateArticle(id uint32) error {
+	article := model.Article{ID: id}
+	return article.Update(d.engine)
+}
+
+// 删除指定文章...
+func (d *Dao) DeleteArticle(id uint32) error {
+	article := model.Article{ID: id}
+
+	return article.Delete(d.engine)
 }
