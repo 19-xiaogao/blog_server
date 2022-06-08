b1dca60fca7d4f5bfcebfd686a07ae2a9287dc4d
xiaolong <longjiuwei999@163.com>
2022-06-08 16:58:49 +0800

    write article create function

--- internal/dao/article.go
+++ internal/dao/article.go
@@ -1,6 +1,10 @@
 package dao
 
-import "xiaolong_blog/internal/model"
+import (
+	"strconv"
+	"time"
+	"xiaolong_blog/internal/model"
+)
 
 // 计算所有文章的总数
 func (d *Dao) CountArticle() (int, error) {
@@ -8,8 +12,9 @@ func (d *Dao) CountArticle() (int, error) {
 	return article.AllCount(d.engine)
 }
 
-func (d *Dao) UpdateArticle(id uint32) error {
-	article := model.Article{ID: id}
+// 更新指定文章
+func (d *Dao) UpdateArticle(id uint32, title string, describe string, content string, musicUrl string, coverUrl string) error {
+	article := model.Article{ID: id, Title: title, Describe: describe, CoverUrl: coverUrl, Content: content, MusicUrl: musicUrl}
 	return article.Update(d.engine)
 }
 
@@ -18,4 +23,21 @@ func (d *Dao) DeleteArticle(id uint32) error {
 	article := model.Article{ID: id}
 
 	return article.Delete(d.engine)
+}
+
+// 创建文章
+func (d *Dao) CreateArticle(title string, describe string, content string, musicUrl string, coverUrl string) error {
+	article := model.Article{
+		Title:        title,
+		Describe:     describe,
+		Content:      content,
+		MusicUrl:     musicUrl,
+		CoverUrl:     coverUrl,
+		CreateTime:   strconv.FormatInt(time.Now().Unix(), 10),
+		CommentId:    0,
+		LickCount:    0,
+		LookCount:    0,
+		CommentCount: 0,
+	}
+	return article.Create(d.engine)
 }
