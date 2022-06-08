41e510354b83551b53a0d07e8a5fc5795755fae3
xiaolong <longjiuwei999@163.com>
2022-06-08 16:58:01 +0800

    update err code

--- pkg/errcode/common_code.go
+++ pkg/errcode/common_code.go
@@ -4,7 +4,7 @@ var (
 	Success                   = NewError(0, "成功")
 	ServerError               = NewError(10000000, "服务器内部错误")
 	InvalidParams             = NewError(10000001, "入参错误")
-	NotFound                  = NewError(10000001, "找不到")
+	NotFound                  = NewError(10000002, "找不到")
 	UnauthorizedAuthNotExist  = NewError(10000003, "鉴权失败，找不到对应的 AppKey 和 AppSecret")
 	UnauthorizedTokenError    = NewError(10000004, "鉴权失败，Token 错误")
 	UnauthorizedTokenTimeout  = NewError(10000005, "鉴权失败，Token 超时")
