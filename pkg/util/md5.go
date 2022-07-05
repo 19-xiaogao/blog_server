0ff58599a2cafcaf7d447c0029c405229afff087
xiaolong <longjiuwei999@163.com>
2022-07-05 11:39:46 +0800

    add random number function

--- pkg/util/md5.go
+++ pkg/util/md5.go
@@ -3,10 +3,21 @@ package util
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"math/rand"
 )
 
 func EncodeMD5(value string) string {
 	m := md5.New()
 	m.Write([]byte(value))
 	return hex.EncodeToString(m.Sum(nil))
+}
+
+var letters = []rune("123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func CreateSixNumber() string {
+	b := make([]rune, 6)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
