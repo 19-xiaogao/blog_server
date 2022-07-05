2b0727f10213762ca2c21c700dcaf256310e4392
xiaolong <longjiuwei999@163.com>
2022-07-05 11:40:34 +0800

    add email send fail

--- pkg/errcode/module_code.go
+++ pkg/errcode/module_code.go
@@ -1,12 +1,13 @@
 package errcode
 
 var (
-	ErrorGetArticleListFail   = NewError(20010001, "获取文章列表失败")
-	ErrorCreateArticleFail    = NewError(20010002, "创建文章失败")
-	ErrorUpdateArticleFail    = NewError(20010003, "更新文章失败")
-	ErrorDeleteArticleFail    = NewError(20010004, "删除文章失败")
-	ErrorCountArticleFail     = NewError(20010005, "统计文章失败")
-	ErrorUploadFileFail       = NewError(20030001, "上传文件失败")
-	ErrorLoginTokenFileFail   = NewError(20030002, "token生成失败")
-	ErrorLoginNotExitUserFail = NewError(20030003, "没有查询到用户")
+	ErrorGetArticleListFail    = NewError(20010001, "获取文章列表失败")
+	ErrorCreateArticleFail     = NewError(20010002, "创建文章失败")
+	ErrorUpdateArticleFail     = NewError(20010003, "更新文章失败")
+	ErrorDeleteArticleFail     = NewError(20010004, "删除文章失败")
+	ErrorCountArticleFail      = NewError(20010005, "统计文章失败")
+	ErrorUploadFileFail        = NewError(20030001, "上传文件失败")
+	ErrorLoginTokenFileFail    = NewError(20030002, "token生成失败")
+	ErrorLoginNotExitUserFail  = NewError(20030003, "没有查询到用户")
+	ErrorRegisterSendEmailFail = NewError(20030005, "邮件发送失败")
 )
